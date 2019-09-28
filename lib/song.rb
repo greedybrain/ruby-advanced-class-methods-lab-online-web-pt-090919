4a3fb3f94e864b4cd5716531a3821f29d06d713c
Naya  Willis <greedybrain18@gmail.com>
2019-09-28 17:46:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,40 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create 
+    song = self.new
+    song.save 
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    song
+    @@all << song
+  end
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
